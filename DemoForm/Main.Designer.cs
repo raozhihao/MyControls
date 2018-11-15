fe9d866557c8df241c6b52a5cbe94d1a4b2536ef
raozhihao <raozhihao2014@hotmail.com>
2018-11-15 12:16:31 +0800

    提交第一个控件,很简单的一个文本框控件,就是一条线做底, 线条可更改颜色和粗细度,可更改控件的背景前景字体等

---
+++ DemoForm/Main.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DemoForm
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.myTextLine1 = new MyTextLine.MyTextLine();
+            this.myTextLine2 = new MyTextLine.MyTextLine();
+            this.myTextLine3 = new MyTextLine.MyTextLine();
+            this.myTextLine4 = new MyTextLine.MyTextLine();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.myTextLine5 = new MyTextLine.MyTextLine();
+            this.SuspendLayout();
+            // 
+            // myTextLine1
+            // 
+            this.myTextLine1.LineBold = 0;
+            this.myTextLine1.Location = new System.Drawing.Point(190, 46);
+            this.myTextLine1.Name = "myTextLine1";
+            this.myTextLine1.PassWordChar = '\0';
+            this.myTextLine1.Size = new System.Drawing.Size(75, 23);
+            this.myTextLine1.TabIndex = 0;
+            this.myTextLine1.Text = "myTextLine1";
+            // 
+            // myTextLine2
+            // 
+            this.myTextLine2.Location = new System.Drawing.Point(190, 109);
+            this.myTextLine2.Name = "myTextLine2";
+            this.myTextLine2.PassWordChar = '\0';
+            this.myTextLine2.Size = new System.Drawing.Size(75, 23);
+            this.myTextLine2.TabIndex = 1;
+            this.myTextLine2.Text = "myTextLine2";
+            // 
+            // myTextLine3
+            // 
+            this.myTextLine3.Location = new System.Drawing.Point(190, 172);
+            this.myTextLine3.Name = "myTextLine3";
+            this.myTextLine3.PassWordChar = '*';
+            this.myTextLine3.Size = new System.Drawing.Size(75, 23);
+            this.myTextLine3.TabIndex = 2;
+            this.myTextLine3.Text = "myTextLine3";
+            // 
+            // myTextLine4
+            // 
+            this.myTextLine4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.myTextLine4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.myTextLine4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.myTextLine4.LineBold = 10;
+            this.myTextLine4.LineColor = System.Drawing.Color.DarkRed;
+            this.myTextLine4.Location = new System.Drawing.Point(190, 235);
+            this.myTextLine4.Name = "myTextLine4";
+            this.myTextLine4.PassWordChar = '\0';
+            this.myTextLine4.Size = new System.Drawing.Size(151, 43);
+            this.myTextLine4.TabIndex = 3;
+            this.myTextLine4.Text = "myTextLine4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "无任何效果";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "初始状态";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码状态";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "花哨状态";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "自动调整状态";
+            // 
+            // myTextLine5
+            // 
+            this.myTextLine5.Font = new System.Drawing.Font("微软雅黑", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.myTextLine5.LineBold = 5;
+            this.myTextLine5.LineColor = System.Drawing.Color.DarkRed;
+            this.myTextLine5.Location = new System.Drawing.Point(190, 318);
+            this.myTextLine5.MyAutoSize = true;
+            this.myTextLine5.Name = "myTextLine5";
+            this.myTextLine5.PassWordChar = '\0';
+            this.myTextLine5.Size = new System.Drawing.Size(111, 28);
+            this.myTextLine5.TabIndex = 5;
+            this.myTextLine5.Text = "myTextLine5";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(834, 522);
+            this.Controls.Add(this.myTextLine5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.myTextLine4);
+            this.Controls.Add(this.myTextLine3);
+            this.Controls.Add(this.myTextLine2);
+            this.Controls.Add(this.myTextLine1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MyTextLine.MyTextLine myTextLine1;
+        private MyTextLine.MyTextLine myTextLine2;
+        private MyTextLine.MyTextLine myTextLine3;
+        private MyTextLine.MyTextLine myTextLine4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private MyTextLine.MyTextLine myTextLine5;
+    }
+}
+
---
+++ MyTextLine/MyTextLine.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTextLine
+{
+    public class MyTextLine : Control
+    {
+        /// <summary>
+        /// 设置控件的宽度和高度是否随文字长度更改而改变
+        /// </summary>
+        private bool autoSize;
+
+        
+        /// <summary>
+        /// 控件的宽度是否随文字长度而更改
+        /// </summary>
+        [
+           Category ("布局"),
+           DefaultValue (false),
+           Description ("设置控件的宽度和高度是否随文字长度更改而改变"),
+           Browsable (true),
+       ]
+        public bool MyAutoSize
+        {
+            get
+            {
+                return autoSize;
+            }
+            set
+            {
+                autoSize = value;
+               // TextColorChanged?.Invoke (this , ea);
+                //更改了此值后判断
+                if ( value )
+                {
+                    SetAutoSize ();
+                }
+                //刷新控件
+                Invalidate ();
+            }
+        }
+
+        /// <summary>
+        /// 线条的颜色
+        /// </summary>
+        private Color lineColor;
+
+        /// <summary>
+        /// 线条颜色改变事件
+        /// </summary>
+        [
+           Category ("属性已更改"),
+           Description ("线条颜色改变事件"),
+           Browsable (true),
+           DesignOnly (false)
+       ]
+        public event Action<object , EventArgs> LineColorChanged;
+       
+        /// <summary>
+        /// 线条的颜色
+        /// </summary>
+        [
+            Category ("外观"),
+             DefaultValue (typeof (Color) , "Black"),
+            Description ("线条的颜色设置")
+        ]
+        public Color LineColor
+        {
+            get
+            {
+                return lineColor;
+            }
+            set
+            {
+                lineColor = value;
+                LineColorChanged?.Invoke (this , new EventArgs());
+                Invalidate ();
+            }
+        }
+
+        /// <summary>
+        /// 线条粗细度已更改
+        /// </summary>
+        [
+            Category ("属性已更改"),
+            Description ("线条粗细度已更改")
+        ]
+        public event Action<object , EventArgs> LineWidthChanged;
+
+        /// <summary>
+        /// 线条粗细度
+        /// </summary>
+        private int lineBold;
+
+        private TextBox LineBox;
+
+
+        /// <summary>
+        /// 线条粗细度
+        /// </summary>
+        [
+          Category ("外观"),
+             DefaultValue (typeof (int) , "1"),
+          Description ("线条粗细度更改")
+      ]
+        public int LineBold
+        {
+            get
+            {
+                return lineBold;
+            }
+            set
+            {
+                lineBold = value;
+                LineWidthChanged?.Invoke (this , new EventArgs ());
+                SetAutoSize ();
+                Invalidate ();
+            }
+        }
+
+        /// <summary>
+        /// 不可编辑
+        /// </summary>
+        [
+          Category ("行为"),
+             DefaultValue (false),
+          Description ("线条粗细度更改")
+      ]
+        public bool ReadOnly
+        {
+            get; set;
+        } = false;
+        
+        private char passWordChar;
+        /// <summary>
+        /// 指示将输入的字符替换成指定的字符
+        /// </summary>
+        [
+          Category ("行为"),
+          Description ("线条粗细度更改")
+      ]
+        public char PassWordChar
+        {
+            get
+            {
+                return passWordChar;
+            }
+            set
+            {
+                passWordChar = value;
+                Invalidate ();
+            }
+        }
+
+        /// <summary>
+        /// 重写Text
+        /// </summary>
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+
+            set
+            {
+                base.Text = value;
+                Invalidate ();
+            }
+        }
+        
+
+        public MyTextLine ()
+        {
+            //初始化一些参数
+            lineColor = Color.Black;
+            lineBold = 1;
+            InitializeComponent ();
+        }
+        
+        /// <summary>
+        /// 重写
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint (PaintEventArgs e)
+        {
+            base.OnPaint (e);
+            using ( Graphics g = e.Graphics )
+            {
+                //将当前Text暂存
+                string input = this.Text;
+               
+                //画上文字之前判断是否有替换符
+                if ( passWordChar != '\0' )
+                {
+                    string pass = string.Empty;
+                    //如果有,就将Text替换一下
+                    for ( int i = 0; i < Text.Length; i++ )
+                    {
+                        pass += PassWordChar;
+                    }
+                    //如果有替换符,则将暂存变量全替换成替换符
+                    //以备等会画上去
+                    input = pass;
+                   
+                }
+                else
+                {
+                    input = Text;
+                    
+                }
+
+                //获取当前字体的相关尺寸信息
+                SizeF fontSize = g.MeasureString (Text , Font);
+                //设置当前Text应该画在控件的高度的位置
+                float drawHeighPoint = 0;
+                //如果当前控件的高度大于文字的高度
+                if ( (this.Size.Height-lineBold ) > fontSize.Height )
+                {
+                    //则应让文字在控件内垂直居中
+                    drawHeighPoint = ( this.Height - fontSize.Height-lineBold ) / 2;
+                }
+                //创建一个画刷对象
+                SolidBrush sb = new SolidBrush (ForeColor);
+                
+               
+                //画上文字
+                g.DrawString (input , Font , sb , 0 , drawHeighPoint);
+
+                //创建画笔对象
+                Pen pen = new Pen (LineColor);
+                pen.Width = lineBold;
+                
+                //如果当前的自动大小更改打开了的话
+                float drawLineHeight = 0;
+                if ( autoSize )
+                {
+                    this.Width = (int)fontSize.Width;
+                    this.Height = (int)fontSize.Height + lineBold;
+                    this.Size = new Size (Width , Height);
+                    drawLineHeight =(int)fontSize.Height;
+                }
+                else
+                {
+                    drawLineHeight = this.Height - lineBold;
+                }
+                //画上线条
+                g.FillRectangle (new SolidBrush (lineColor) , 0 , drawLineHeight , this.Width , lineBold);
+                
+            }
+           
+        }
+
+
+      
+
+        /// <summary>
+        /// 重写鼠标按下函数
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseDown (MouseEventArgs e)
+        {
+            base.OnMouseDown (e);
+
+            if ( ReadOnly )
+            {
+                //如果选择了不可编辑,则取消
+                return;
+            }
+            //当鼠标点击的时候,让其可编辑
+            //思路就是参照DataGridVeiw中的编辑功能
+            //在当前控件的位置放置一个TextBox
+            LineBox_Enter (LineBox , new EventArgs ());
+            
+        }
+
+        private void Tb_KeyPress (object sender , KeyPressEventArgs e)
+        {
+            //如果当前按下的是Enter键
+            if ( e.KeyChar == 13 )
+            {
+                e.Handled = true;
+            }
+        }
+       
+        protected override void OnEnter (EventArgs e)
+        {
+            base.OnEnter (e);
+            
+            //调用控件中的文本框进入事件
+            LineBox_Enter (LineBox,new EventArgs());
+        }
+        protected override void OnLeave (EventArgs e)
+        {
+            base.OnLeave (e);
+            //调用控件中的文本框失去焦点事件
+            LineBox_Leave (LineBox , new EventArgs ());
+        }
+
+        protected override void OnFontChanged (EventArgs e)
+        {
+            base.OnFontChanged (e);
+            //字体更改时重置控件大小
+            SetAutoSize ();
+            
+        }
+
+        protected override void OnTextChanged (EventArgs e)
+        {
+            base.OnTextChanged (e);
+            //文字更改时重置控件大小
+            SetAutoSize ();
+        }
+
+       
+
+        /// <summary>
+        /// 设置自动尺寸
+        /// </summary>
+        private void SetAutoSize ()
+        {
+            //确定当前是否需要自适应宽度
+            if ( autoSize )
+            {
+                //获取当前字体的相关尺寸信息
+                SizeF fontSize = GetFontSize (Text , Font);
+                
+                this.Width = (int)fontSize.Width;
+                this.Height = (int)fontSize.Height+lineBold;
+                this.Size = new Size (Width , Height);
+                //重绘控件
+                Invalidate ();
+            }
+            
+
+        }
+
+        /// <summary>
+        /// 获取指定文字的字体尺寸信息
+        /// </summary>
+        /// <param name="getSizeText">需要获取的文字</param>
+        /// <param name="font">该文字当前使用的字体</param>
+        /// <returns></returns>
+        private SizeF GetFontSize (string getSizeText , Font font)
+        {
+            SizeF fontSize;
+            //获取绘图对象
+            using ( Graphics g = Graphics.FromHwnd (this.Handle) )
+            {
+                //获取当前字体的相关尺寸信息
+                fontSize = g.MeasureString (getSizeText , font);
+            }
+            return fontSize;
+        }
+
+        
+        private void InitializeComponent ()
+        {
+            this.LineBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LineBox
+            // 
+            this.LineBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.LineBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LineBox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.LineBox.Location = new System.Drawing.Point(0, 0);
+            this.LineBox.Name = "LineBox";
+            this.LineBox.Size = new System.Drawing.Size(100, 14);
+            this.LineBox.TabIndex = 0;
+            this.LineBox.Visible = false;
+            
+            // 
+            // MyTextLine
+            // 
+            this.Controls.Add(this.LineBox);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void LineBox_Enter (object sender , EventArgs e)
+        {
+            //先检查是否开启了密码替换符功能
+            if ( passWordChar != '\0' )
+            {
+                //如果开启则也开启文本框的
+                LineBox.PasswordChar = passWordChar;
+            }
+
+            //显示文本框
+            LineBox.Visible = true;
+            //[定位文本框
+            LineBox.Location =new Point(0, ( this.Height - LineBold - LineBox.Height ) / 2);
+            //设置文本框的一些其它属性
+            LineBox.Width = this.Width;
+            LineBox.Text = this.Text;
+            LineBox.BackColor = this.BackColor;
+            LineBox.ForeColor = this.ForeColor;
+            LineBox.Font = this.Font;
+            //使文本框得到焦点
+            LineBox.SelectionStart = LineBox.TextLength;
+            LineBox.Select ();
+        }
+
+        private void LineBox_Leave (object sender , EventArgs e)
+        {
+            LineBox.Visible = false;
+            this.Text = LineBox.Text;
+        }
+    }
+}
